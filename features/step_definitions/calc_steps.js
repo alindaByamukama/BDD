5ab9d1bafb6d0ce68bd02da03b753676c46d7ec4
alindaByamukama <alindabyamukama@gmail.com>
2023-03-09 23:56:47 +0300

    add changes - when-definition-click-add-button-has-passed

--- features/step_definitions/calc_steps.js
+++ features/step_definitions/calc_steps.js
@@ -3,19 +3,23 @@ const assert = require('assert');
 const { By, Key } = require("selenium-webdriver");
 const webdriver = require('selenium-webdriver');
 require('chromedriver');
-let driver = driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-const { setDefaultTimeout } = require('@cucumber/cucumber');
-setDefaultTimeout(60 * 1000);
+// const { setDefaultTimeout } = require('@cucumber/cucumber');
+// setDefaultTimeout(60 * 1000);
 
 const Calculator = require("../../lib/calc");
 
-// Before(function () {
-// })
+let driver;
+Before(function () {
+    driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    // .setChromeOptions(/* ... */)
+    // .setFirefoxOptions(/* ... */)
+    .build();
+})
 
-// After(function () {
-// })
+After(function () {
+    driver.quit();
+})
 
 Given ('the numbers {int} and {int}', function (x, y) {
     calculator = new Calculator(x, y)
@@ -41,24 +45,27 @@ Then('result is {int}', function (result) {
     assert.equal(calculator.getResult(), result)
 });
 
-Given('I visit the calculator app page', async () => {
+Given('I visit the calculator app page',{timeout: 60 * 1000}, async () => {
     await driver.get('http://207.154.255.199:4300/');
 });
 
-When('I enter a number in num1', async () => {
+When('I enter a number in num1',{timeout: 60 * 1000}, async () => {
     let num1 = await driver.findElement(By.id('num1'))
     num1.sendKeys(Key.ENTER, "4", Key.NULL)
 });
 
-When('I enter a number in num2', async () => {
+When('I enter a number in num2',{timeout: 60 * 1000}, async () => {
     let num2 = await driver.findElement(By.id('num2'))
     num2.sendKeys(Key.ENTER, "4", Key.NULL)
 });
 
-When('I click the add button', asyc () => {
-    
+When('I click the add button',{timeout: 60 * 1000}, async () => {
+    let addButton = await driver.findElement(By.id('addition'))
+    addButton.click()
 });
 
-Then('the result of num1 added to num2 is displayed', async () => {
-    
+Then('the result of num1 added to num2 is displayed',{timeout: 60 * 1000}, async () => {
+    let result = await driver.findElement(By.id('result'))
+    result.getText()
+    console.log(result);
 });
